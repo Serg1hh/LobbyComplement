5a1571a9b568620e7a06b60190c247af031c3d29
Serg1hh <94806331+Serg1hh@users.noreply.github.com>
2022-01-27 16:53:33 -0300

    pm4

--- LobbyComplement/src/lobbycomplement/serg1h/LobbyComplement.php
+++ LobbyComplement/src/lobbycomplement/serg1h/LobbyComplement.php
@@ -10,20 +10,21 @@ use pocketmine\plugin\PluginBase;
 
 class LobbyComplement extends PluginBase implements Listener {
 
-    public function onEnable() {
+    protected function onEnable(): void {
+
+        $this->getServer()->getLogger()->info("LobbyComplement has been enabled!");
 
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveDefaultConfig();
         $this->getServer()->getCommandMap()->register("fly", new FlyCommand($this));
     }
 
     public function onExhaust(PlayerExhaustEvent $event): void {
-            $event->setCancelled();  // default is true
+            $event->cancel();  // default is true
         }
 
     public function onDamage(EntityDamageEvent $event) {
         if ($event->getCause() === EntityDamageEvent::CAUSE_FALL) {
-            $event->setCancelled(); // default is true
+            $event->cancel(); // default is true
         }
     }
 }
